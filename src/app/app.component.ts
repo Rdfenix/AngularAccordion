4a2a8d6f7146659612c63871d013fb8c076dab20
rdfenix <rhoodsk8erboy@gmail.com>
2020-11-03 22:47:49 -0300

    accordion created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'AccordionAngular';
+  @ViewChildren('accordion') teste: QueryList<ElementRef>;
+
+  constructor() {}
+
+  funcTest(i: number): void {
+    const el = this.teste.toArray().filter((_item, index) => index === i)[0];
+    el.nativeElement.classList.toggle('active');
+    let content = el.nativeElement.nextElementSibling;
+
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + 'px';
+    }
+  }
 }
